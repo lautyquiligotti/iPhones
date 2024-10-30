07c3c2411d13f530ec50f5c591ebdfa0a1617b55
Lautaro Quiligotti <lautyquiligotti3@gmail.com>
2024-10-30 16:07:36 -0300

    fotos
    fotos

--- index.js
+++ index.js
@@ -4,79 +4,79 @@ const inventario = { // Objeto con la lista de productos
             nombre: "iPhone 11 ", 
             Almacenamiento: 128, 
             Precio: 300, 
-            imagen: "https://buenosairesimport.com/2464-large_default/iphone-11-128-gb.jpg" // URL de la imagen
+            imagen: "./Recursos/Imagenes/iPhone_11.png" // URL de la imagen
         },
         { 
             nombre: "iPhone 11 Pro", 
             Almacenamiento: 128, 
             Precio: 380, 
-            imagen: "https://www.apple.com/newsroom/images/product/iphone/standard/Apple_iPhone-11-Pro_Colors_091019_big.jpg.large.jpg" // URL de la imagen
+            imagen: "./Recursos/Imagenes/iPhone_11Pro.png" // URL de la imagen
         },
         { 
             nombre: "iPhone 12", 
             Almacenamiento: 128, 
             Precio: 390, 
-            imagen: "https://acdn.mitiendanube.com/stores/001/590/690/products/diseno-sin-titulo-911-1f3219897c4971f84d16449610963425-1024-1024.png" // URL de la imagen
+            imagen: "./Recursos/Imagenes/iPhone_12.png" // URL de la imagen
         },
         { 
             nombre: "iPhone 12 Pro ", 
             Almacenamiento: 128, 
             Precio: 430, 
-            imagen: "https://www.compumacypc.com.ar/wp-content/uploads/Apple_iphone12mini-iphone12max-homepodmini-availability_iphone12promax-geo_110520_inline.jpg.large-1.jpg" // URL de la imagen
+            imagen: "./Recursos/Imagenes/iPhone_12Pro.png" // URL de la imagen
         },
         { 
             nombre: "iPhone 13", 
             Almacenamiento: 128, 
             Precio: 500, 
-            imagen: "https://www.apple.com/newsroom/images/product/iphone/standard/Apple-iPhone13-color-lineup-220308_big_carousel.jpg.small_2x.jpg" // URL de la imagen
+            imagen: "./Recursos/Imagenes/iPhone_13.webp" // URL de la imagen
         },
         { 
             nombre: "iPhone 13 Pro", 
             Almacenamiento: 128, 
             Precio: 600, 
-            imagen: "https://i0.wp.com/sixcolors.com/wp-content/uploads/2021/10/iphone13pro-lineup-new-6c.jpg?ssl=1" // URL de la imagen
+            imagen: "./Recursos/Imagenes/iPhone_13Pro.png" // URL de la imagen
         },
         { 
             nombre: "iPhone 14 ", 
             Almacenamiento: 128, 
             Precio: 570, 
-            imagen: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/refurb-iphone-14-plus-midnight-202404_AV1_GEO_EMEA?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1713205107544" // URL de la imagen
+            imagen: "./Recursos/Imagenes/iPhone_14.png" // URL de
         },
         { 
             nombre: "iPhone 14 Pro ", 
             Almacenamiento: 128, 
             Precio: 780, 
-            imagen: "https://www.macstation.com.ar/web/image/product.image/1985/image_1024/iPhone%2014%20Pro%20Max%20128GB%20-%20Plata?unique=d9de110" // URL de la imagen
+            imagen: "./Recursos/Imagenes/iPhone_14Pro.png" // URL de la imagen
         },
         { 
             nombre: "iPhone 15 ", 
             Almacenamiento: 128, 
             Precio: 800, 
-            imagen: "https://cdsassets.apple.com/live/7WUAS350/images/tech-specs/iphone_15_hero.png" // URL de la imagen
+            imagen: "./Recursos/Imagenes/iPhone_15.webp" // URL de la imagen
         },
         { 
             nombre: "iPhone 15 Pro", 
             Almacenamiento: 128, 
             Precio: 970, 
-            imagen: "https://cdsassets.apple.com/live/7WUAS350/images/tech-specs/iphone_15_pro.png" // URL de la imagen
+            imagen: "./Recursos/Imagenes/iPhone_15Pro.png" // URL de la imagen
         },
         { 
             nombre: "iPhone 16", 
             Almacenamiento: 128, 
             Precio: 1000, 
-            imagen: "https://d28hi93gr697ol.cloudfront.net/9ef84dda-32dd-4016-7da3-1c0a824fffb4/seo/Iphone-16-colores-66eec9bfa7bc6-O.png" // URL de la imagen
+            imagen: "./Recursos/Imagenes/iPhone_16.png" // URL de la imagen
         },
         { 
             nombre: "iPhone 16 Pro", 
             Almacenamiento: 128, 
             Precio: 1280, 
-            imagen: "https://cdsassets.apple.com/live/7WUAS350/images/tech-specs/121031-iphone-16-pro.png"
+            imagen: "./Recursos/Imagenes/iPhone_16Pro.png"
         },
         { 
             nombre: "iPhone 16 Pro Max", 
             Almacenamiento: 256, 
             Precio: 1550, 
-            imagen: "https://cdsassets.apple.com/live/7WUAS350/images/tech-specs/121032-iphone-16-pro-max.png"
+            imagen: "./Recursos/Imagenes/iPhone_16Pro.png"
         },        
     ]
 };
