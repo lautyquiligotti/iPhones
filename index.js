0c2a56a557580839c06c39f83f005598ecd9e4d8
Lautaro Quiligotti <lautyquiligotti3@gmail.com>
2024-11-06 17:15:39 -0300

    6/11
    6/11

--- index.js
+++ index.js
@@ -4,79 +4,79 @@ const inventario = { // Objeto con la lista de productos
             nombre: "iPhone 11 ", 
             Almacenamiento: 128, 
             Precio: 300, 
-            imagen: "./Recursos/Imagenes/iPhone_11.png" // URL de la imagen
+            imagen: "./Recursos/Imagenes/iPhone_11.png" // Ruta relativa (fotos locales)
         },
         { 
             nombre: "iPhone 11 Pro", 
             Almacenamiento: 128, 
             Precio: 380, 
-            imagen: "./Recursos/Imagenes/iPhone_11Pro.png" // URL de la imagen
+            imagen: "./Recursos/Imagenes/iPhone_11Pro.png" // Ruta relativa (fotos locales)
         },
         { 
             nombre: "iPhone 12", 
             Almacenamiento: 128, 
             Precio: 390, 
-            imagen: "./Recursos/Imagenes/iPhone_12.png" // URL de la imagen
+            imagen: "./Recursos/Imagenes/iPhone_12.png" // Ruta relativa (fotos locales)
         },
         { 
             nombre: "iPhone 12 Pro ", 
             Almacenamiento: 128, 
             Precio: 430, 
-            imagen: "./Recursos/Imagenes/iPhone_12Pro.png" // URL de la imagen
+            imagen: "./Recursos/Imagenes/iPhone_12Pro.png" // Ruta relativa (fotos locales)
         },
         { 
             nombre: "iPhone 13", 
             Almacenamiento: 128, 
             Precio: 500, 
-            imagen: "./Recursos/Imagenes/iPhone_13.webp" // URL de la imagen
+            imagen: "./Recursos/Imagenes/iPhone_13.webp" // Ruta relativa (fotos locales)
         },
         { 
             nombre: "iPhone 13 Pro", 
             Almacenamiento: 128, 
             Precio: 600, 
-            imagen: "./Recursos/Imagenes/iPhone_13Pro.png" // URL de la imagen
+            imagen: "./Recursos/Imagenes/iPhone_13Pro.png" // Ruta relativa (fotos locales)
         },
         { 
             nombre: "iPhone 14 ", 
             Almacenamiento: 128, 
             Precio: 570, 
-            imagen: "./Recursos/Imagenes/iPhone_14.png" // URL de
+            imagen: "./Recursos/Imagenes/iPhone_14.png" // Ruta relativa (fotos locales)
         },
         { 
             nombre: "iPhone 14 Pro ", 
             Almacenamiento: 128, 
             Precio: 780, 
-            imagen: "./Recursos/Imagenes/iPhone_14Pro.png" // URL de la imagen
+            imagen: "./Recursos/Imagenes/iPhone_14Pro.png" // Ruta relativa (fotos locales)
         },
         { 
             nombre: "iPhone 15 ", 
             Almacenamiento: 128, 
             Precio: 800, 
-            imagen: "./Recursos/Imagenes/iPhone_15.webp" // URL de la imagen
+            imagen: "./Recursos/Imagenes/iPhone_15.webp" // Ruta relativa (fotos locales)
         },
         { 
             nombre: "iPhone 15 Pro", 
             Almacenamiento: 128, 
             Precio: 970, 
-            imagen: "./Recursos/Imagenes/iPhone_15Pro.png" // URL de la imagen
+            imagen: "./Recursos/Imagenes/iPhone_15Pro.png" // Ruta relativa (fotos locales)
         },
         { 
             nombre: "iPhone 16", 
             Almacenamiento: 128, 
             Precio: 1000, 
-            imagen: "./Recursos/Imagenes/iPhone_16.png" // URL de la imagen
+            imagen: "./Recursos/Imagenes/iPhone_16.png" // Ruta relativa (fotos locales)
         },
         { 
             nombre: "iPhone 16 Pro", 
             Almacenamiento: 128, 
             Precio: 1280, 
-            imagen: "./Recursos/Imagenes/iPhone_16Pro.png"
+            imagen: "./Recursos/Imagenes/iPhone_16Pro.png" // Ruta relativa (fotos locales)
         },
         { 
             nombre: "iPhone 16 Pro Max", 
             Almacenamiento: 256, 
             Precio: 1550, 
-            imagen: "./Recursos/Imagenes/iPhone_16Pro.png"
+            imagen: "./Recursos/Imagenes/iPhone_16Pro.png" // Ruta relativa (fotos locales)
         },        
     ]
 };
